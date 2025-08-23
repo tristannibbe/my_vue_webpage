e39b202de3cc50a9406fd254f6e2c058c3d3d7c8
Tristan Nibbe <tristannibbe96@gmail.com>
2025-08-23 00:16:09 -0700

    Added collapsing

--- src/cailas_page.vue
+++ src/cailas_page.vue
@@ -1,6 +1,5 @@
 <script>
 import { activity } from './activity'
-import { RouterLink, RouterView } from 'vue-router'
 
 export default
 {
@@ -47,6 +46,24 @@ export default
 
             var encodedUri = encodeURI(csvContent);
             window.open(encodedUri);
+        },
+        toggle_row()
+        {
+            console.log("toggle_row enter")
+            var elements = document.querySelectorAll(".accordion")
+
+            elements.forEach(function(element)
+            {
+
+                element.classList.toggle("collapse")
+                
+            })
+
+            //var elements = document.querySelectorAll(".expand_button")
+            //elements.forEach(function(element)
+            //{
+            //    element.textContent = ">"
+            //})
         }
     }
 }
@@ -54,10 +71,8 @@ export default
 
 <template>
     <h1>Caila's Page</h1>
-    <RouterView />
     <table>
         <tr>
-            <th>Date</th>
             <th>Description</th>
             <th>Location</th>
             <th>Code</th>
@@ -65,36 +80,36 @@ export default
             <th>Hours</th>
             <th></th>
         </tr>
-
-        <tr v-for="entry in activities">
-            <td><input type="date" v-model="entry.date" /></td>
-            <td><input v-model="entry.description" placeholder="Enter Activity" /></td>
-            <td><input v-model="entry.location" placeholder="Enter City" /></td>
-
-            <td class="single_character_cell">
-                <select class="single_character_cell" v-model="entry.activity_code">
-                    <option disabled value=""></option>
-                    <option>A</option>
-                    <option>B</option>
-                    <option>C</option>
-                </select>
-            </td>
-
-            <td><input v-model="entry.people_present" class="single_character_cell" type="number" value="1"/></td>
-            <td><input v-on:input="total_hours_spent" step="0.5" class="single_character_cell" v-model="entry.time_spent" type="number" value="1.0"/></td>
-            <td><button @click="delete_entry(entry)">X</button></td>
-        </tr>
-
+        
         <tr>
+
+            <td><button class="expand_button" @click="toggle_row">Expand/Collapse</button></td>
             <td><input type="date" v-model="date" placeholder="Enter Date"/></td>
-            <td></td>
-            <td></td>
             <td></td>
             <td></td>
             <td><div>Total:{{ total_hours }}</div></td>
             <td><button @click="add_entry">+</button></td>
-
         </tr>
+
+        <tr class="accordion" v-for="entry in activities">
+
+                <td><input v-model="entry.description" placeholder="Enter Activity" /></td>
+                <td><input v-model="entry.location" placeholder="Enter City" /></td>
+
+                <td class="single_character_cell">
+                    <select class="single_character_cell" v-model="entry.activity_code">
+                        <option disabled value=""></option>
+                        <option>A</option>
+                        <option>B</option>
+                        <option>C</option>
+                    </select>
+                </td>
+
+                <td><input v-model="entry.people_present" class="single_character_cell" type="number" value="1"/></td>
+                <td><input v-on:input="total_hours_spent" step="0.5" class="single_character_cell" v-model="entry.time_spent" type="number" value="1.0"/></td>
+                <td><button @click="delete_entry(entry)">X</button></td>
+        </tr>
+
     </table>
     <button @click="construct_csv">Download</button>
 
