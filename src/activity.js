c31a4a68ea30970ddbb383df4d50e03b770d2d44
tnibbz <tnibbz@linux-laptop.lan>
2025-08-23 12:49:10 -0700

    Add nesting to dates

--- src/activity.js
+++ src/activity.js
@@ -1,5 +1,12 @@
 export class activity
 {
+    date = ""
+    description = ""
+    location = ""
+    activity_code = ""
+    people_present = 1
+    time_spent = 1
+
     constructor(date, description, location, activity_code, people_present, time_spent)
     {
         this.date = date 
@@ -13,11 +20,39 @@ export class activity
                 this.people_present + "," +
                 this.time_spent + ","
     }
+}
 
+export class day_of_activities
+{
     date = ""
-    description = ""
-    location = ""
-    activity_code = ""
-    people_present = 1
-    time_spent = 1
+    total_hours = 0;
+    activities = []
+
+    constructor (date)
+    {
+        this.date = date
+        this.activities.push(new activity(this.date))
+    }
+
+    add_entry() {
+        this.activities.push(new activity(this.date))
+        this.calculate_hours()
+    }
+
+    delete_entry(entry)
+    {
+        this.activities = this.activities.filter(function(item)
+        {
+            return item !== entry
+        })
+        this.calculate_hours()
+    }
+
+    calculate_hours()
+    {
+        this.total_hours = 0;
+        for (let index = 0; index < this.activities.length; index++) {
+            this.total_hours += this.activities[index].time_spent;
+        }
+    }
 }
--- src/cailas_page.vue
+++ src/cailas_page.vue
@@ -1,39 +1,15 @@
 <script>
-import { activity } from './activity'
+import { activity, day_of_activities } from './activity'
 
 export default
 {
     data()
     {
         return{
-            date: "2025-06-01",
-            total_hours: 0,
-            activities: [new activity("2025-06-01")],
+            days_of_activities: [new day_of_activities("2025-06-01"), new day_of_activities("2025-06-02")],
         }
     },
     methods: {
-        add_entry() {
-            this.activities.push(new activity(this.date))
-            this.total_hours_spent()
-        },
-
-        total_hours_spent()
-        {
-            this.total_hours = 0;
-            for (let index = 0; index < this.activities.length; index++) {
-                this.total_hours += this.activities[index].time_spent;
-            }
-        },
-
-        delete_entry(entry)
-        {
-            this.activities = this.activities.filter(function(item)
-            {
-                return item !== entry
-            })
-            this.total_hours_spent()
-        },
-
         construct_csv()
         {
             console.log("enter")
@@ -65,50 +41,52 @@ export default
 </script>
 
 <template>
-    <h1>Caila's Page</h1>
-    <table>
-        <tr>
-            <th>Date</th>
-            <th class="medium_cell">Location</th>
-            <th class="wide_cell">Description</th>
-            <th>Code</th>
-            <th>Participants</th>
-            <th class="single_character_cell">Hours</th>
-            <th></th>
-        </tr>
-        
+<h1>Caila's Page</h1>
+<table>
+    <tr>
+        <th>Date</th>
+        <th class="medium_cell">Location</th>
+        <th class="wide_cell">Description</th>
+        <th>Code</th>
+        <th>Participants</th>
+        <th class="single_character_cell">Hours</th>
+        <th></th>
+    </tr>
+
+    <div v-for="day in days_of_activities">
         <tr class="accordion_header">
-            <td><input type="date" v-model="date" placeholder="Enter Date"/></td>
+            <td><input type="date" v-model="day.date" placeholder="Enter Date"/></td>
             <td @click="toggle_row"></td>
             <td @click="toggle_row" class="wide_cell"></td>
             <td @click="toggle_row" class="single_character_cell"></td>
             <td @click="toggle_row"></td>
-            <td @click="toggle_row"><div>Total:{{ total_hours }}</div></td>
-            <td><button @click="add_entry">+</button></td>
-
+            <td @click="toggle_row"><div>Total:{{ day.total_hours }}</div></td>
+            <td><button @click="day.add_entry()">+</button></td>
         </tr>
 
-        <tr class="accordion" v-for="entry in activities">
-                <td></td>
+        <tr class="accordion" v-for="activity in day.activities">
+            <td></td>
 
-                <td class="medium_cell"><input class="medium_cell" v-model="entry.location" placeholder="Enter City" /></td>
-                <td class="wide_cell"><input class="wide_cell" v-model="entry.description" placeholder="Enter Activity" /></td>
+            <td class="medium_cell"><input class="medium_cell" v-model="activity.location" placeholder="Enter City" /></td>
+            <td class="wide_cell"><input class="wide_cell" v-model="activity.description" placeholder="Enter Activity" /></td>
 
-                <td class="single_character_cell">
-                    <select class="single_character_cell" v-model="entry.activity_code">
-                        <option disabled value=""></option>
-                        <option>A</option>
-                        <option>B</option>
-                        <option>C</option>
-                    </select>
-                </td>
+            <td class="single_character_cell">
+                <select class="single_character_cell" v-model="activity.activity_code">
+                    <option disabled value=""></option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                </select>
+            </td>
 
-                <td><input v-model="entry.people_present" class="single_character_cell" type="number" value="1"/></td>
-                <td><input v-on:input="total_hours_spent" step="0.5" class="single_character_cell" v-model="entry.time_spent" type="number" value="1.0"/></td>
-                <td><button @click="delete_entry(entry)">X</button></td>
+            <td><input v-model="activity.people_present" class="single_character_cell" type="number" value="1"/></td>
+            <td><input v-on:input="day.calculate_hours()" step="0.5" class="single_character_cell" v-model="activity.time_spent" type="number" value="1.0"/></td>
+            <td><button @click="day.delete_entry(activity)">X</button></td>
         </tr>
+    </div>
+</table>
 
-    </table>
-    <button @click="construct_csv">Download</button>
+<br></br>
+<button @click="construct_csv">Download</button>
 
 </template>
